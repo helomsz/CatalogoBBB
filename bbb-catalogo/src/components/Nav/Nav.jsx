d20f41d598dc7e058c32125cc065f11e9b8b272c
HELOISA MILITÃO DE SOUZA <heloisa.souza24@senaisp.edu.br>
2026-01-28 16:59:25 -0100

    uhuul

---
+++ bbb-catalogo/src/components/Nav/Nav.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Menu, X, Heart } from "lucide-react";
+import logoBBB from '../../assets/logobbb.png';
+import "./Nav.css";
+
+export default function Navbar() {
+  const [aberto, setAberto] = useState(false);
+
+  return (
+    <header className="containerCabecalho">
+      <nav className="wrapperNavegacao">
+
+        <div className="containerLogo">
+          <img 
+            className="iconeLogo" 
+            src={logoBBB} 
+            alt="Logo do Reality Show Big Brother BR" 
+          />
+        </div>
+
+        <ul className="listaLinks">
+          <li><a href="#hero" className="linkNavegacao">Início</a></li>
+          <li><a href="#about" className="linkNavegacao">Sobre</a></li>
+          <li><a href="#services" className="linkNavegacao">Serviços</a></li>
+          <li><a href="#testimonials" className="linkNavegacao">Depoimentos</a></li>
+        </ul>
+
+        <div className="botaoFavorio">
+            <Heart/>
+            <a href="#">Favoritos</a>
+        </div>
+
+
+        <button onClick={() => setAberto(!aberto)} className="botaoMenu">
+          {aberto ? <X size={24} /> : <Menu size={24} />}
+        </button>
+      </nav>
+
+      <div className={`menuMobile ${aberto ? "menuAberto" : ""}`}>
+        <a onClick={() => setAberto(false)} href="#hero" className="linkMobile">Início</a>
+        <a onClick={() => setAberto(false)} href="#about" className="linkMobile">Sobre</a>
+        <a onClick={() => setAberto(false)} href="#services" className="linkMobile">Serviços</a>
+        <a onClick={() => setAberto(false)} href="#testimonials" className="linkMobile">Depoimentos</a>
+      </div>
+    </header>
+  );
+}
